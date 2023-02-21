656eafc49ec5c0005172f76a87d9b49514474fe9
nsk1207 <nsh.kim@gmail.com>
2023-02-20 20:02:09 -0800

    commit

--- api/database.py
+++ api/database.py
@@ -1,47 +1,47 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const sqlite3 = require('sqlite3').verbose();
-const app = express();
+# const express = require('express');
+# const bodyParser = require('body-parser');
+# const sqlite3 = require('sqlite3').verbose();
+# const app = express();
 
-app.use(bodyParser.json());
+# app.use(bodyParser.json());
 
-// Connect to the SQLite database
-const db = new sqlite3.Database(':memory:');
+# // Connect to the SQLite database
+# const db = new sqlite3.Database(':memory:');
 
-// Create the login table in the database
-db.run(`CREATE TABLE login (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    age INTEGER NOT NULL,
-    pin INTEGER NOT NULL
-)`);
+# // Create the login table in the database
+# db.run(`CREATE TABLE login (
+#     id INTEGER PRIMARY KEY,
+#     name TEXT NOT NULL,
+#     age INTEGER NOT NULL,
+#     pin INTEGER NOT NULL
+# )`);
 
-// Endpoint to create a new login
-app.post('/login', (req, res) => {
-    const { name, age, pin } = req.body;
+# // Endpoint to create a new login
+# app.post('/login', (req, res) => {
+#     const { name, age, pin } = req.body;
 
-    // Validate input
-    if (!name || !age || !pin) {
-        return res.status(400).send({ error: 'Name, age, and pin are required' });
-    }
+#     // Validate input
+#     if (!name || !age || !pin) {
+#         return res.status(400).send({ error: 'Name, age, and pin are required' });
+#     }
 
-    // Insert login into the database
-    db.run(`INSERT INTO login (name, age, pin) VALUES (?, ?, ?)`, [name, age, pin], function(err) {
-        if (err) {
-            return res.status(500).send({ error: err.message });
-        }
-        res.send({ id: this.lastID });
-    });
-});
+#     // Insert login into the database
+#     db.run(`INSERT INTO login (name, age, pin) VALUES (?, ?, ?)`, [name, age, pin], function(err) {
+#         if (err) {
+#             return res.status(500).send({ error: err.message });
+#         }
+#         res.send({ id: this.lastID });
+#     });
+# });
 
-// Endpoint to retrieve login information
-app.get('/login/:id', (req, res) => {
-    const { id } = req.params;
+# // Endpoint to retrieve login information
+# app.get('/login/:id', (req, res) => {
+#     const { id } = req.params;
 
-    // Retrieve login from the database
-    db.get(`SELECT name, age, pin FROM login WHERE id = ?`, [id], (err, row) => {
-        if (err) {
-            return res.status(500).send({ error: err.message });
-        }
-        if (!row) {
-            return res.status(
+#     // Retrieve login from the database
+#     db.get(`SELECT name, age, pin FROM login WHERE id = ?`, [id], (err, row) => {
+#         if (err) {
+#             return res.status(500).send({ error: err.message });
+#         }
+#         if (!row) {
+#             return res.status(
---
+++ api/wordle.py
@@ -0,0 +1,59 @@
+from flask import Blueprint, request, jsonify
+from flask_restful import Api, Resource # used for REST API building
+from datetime import *
+from random import choice
+
+from model.wordles import Wordle
+
+wordle_api = Blueprint('wordle_api', __name__,
+                   url_prefix='/api/wordles')
+
+# API docs https://flask-restful.readthedocs.io/en/latest/api.html
+api = Api(wordle_api)
+
+class WordleAPI:        
+    class _Create(Resource):
+        def post(self):
+            ''' Read data for json body '''
+            body = request.get_json()
+            
+            ''' Avoid garbage in, error checking '''
+            # validate name
+            name = body.get('name')
+            if name is None or len(name) < 1:
+                return {'message': f'Submitter\'s name is missing, or is less than 1 character'}, 210
+            # validate uid
+            score = body.get('score')
+            pin = body.get('pin')
+            if pin is None or len(pin) < 2:
+                return {'message': f'Pin is missing, or is less than 2 characters'}, 210
+
+            ''' #1: Key code block, setup USER OBJECT '''
+            wo = Wordle(name=name, 
+                      score=score)
+            if pin is not None:
+                wo.set_pin(pin)
+            
+            ''' #2: Key Code block to add user to database '''
+            # create user in database
+            wordle = wo.create()
+            # success returns json of user
+            if wordle:
+                return jsonify(wordle.read())
+            # failure returns error
+            return {'message': f'Processed {name}, either a format error or the name "{name}" is a duplicate'}, 210
+
+    class _Read(Resource):
+        def get(self):
+            wordles = Wordle.query.all()    # read/extract all users from database
+            json_ready = [wordle.read() for wordle in wordles]  # prepare output in json
+            return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
+        
+    class _GenerateRandomWord(Resource):
+        def get(self):
+            word_list = ['ABOUT', 'ABOVE', 'ABUSE', 'ACTOR', 'ACUTE', 'ADMIT', 'ADOPT', 'ADULT', 'AFTER', 'AGAIN', 'AGENT', 'AGREE', 'AHEAD', 'ALARM', 'ALBUM', 'ALERT', 'ALIKE', 'ALIVE', 'ALLOW', 'ALONE', 'ALONG', 'ALTER', 'AMONG', 'ANGER', 'ANGLE', 'ANGRY', 'APART', 'APPLE', 'APPLY', 'ARENA', 'ARGUE', 'ARISE', 'ARRAY', 'ASIDE', 'ASSET', 'AUDIO', 'AUDIT', 'AVOID', 'AWARD', 'AWARE', 'BADLY', 'BAKER', 'BASES', 'BASIC', 'BASIS', 'BEACH', 'BEGAN', 'BEGIN', 'BEGUN', 'BEING', 'BELOW', 'BENCH', 'BILLY', 'BIRTH', 'BLACK', 'BLAME', 'BLIND', 'BLOCK', 'BLOOD', 'BOARD', 'BOOST', 'BOOTH', 'BOUND', 'BRAIN', 'BRAND', 'BREAD', 'BREAK', 'BREED', 'BRIEF', 'BRING', 'BROAD', 'BROKE', 'BROWN', 'BUILD', 'BUILT', 'BUYER', 'CABLE', 'CALIF', 'CARRY', 'CATCH', 'CAUSE', 'CHAIN', 'CHAIR', 'CHART', 'CHASE', 'CHEAP', 'CHECK', 'CHEST', 'CHIEF', 'CHILD', 'CHINA', 'CHOSE', 'CIVIL', 'CLAIM', 'CLASS', 'CLEAN', 'CLEAR', 'CLICK', 'CLOCK', 'CLOSE', 'COACH', 'COAST', 'COULD', 'COUNT', 'COURT', 'COVER', 'CRAFT', 'CRASH', 'CREAM', 'CRIME', 'CROSS', 'CROWD', 'CROWN', 'CURVE', 'CYCLE', 'DAILY', 'DANCE', 'DATED', 'DEALT', 'DEATH', 'DEBUT', 'DELAY', 'DEPTH', 'DOING', 'DOUBT', 'DOZEN', 'DRAFT', 'DRAMA', 'DRAWN', 'DREAM', 'DRESS', 'DRILL', 'DRINK', 'DRIVE', 'DROVE', 'DYING', 'EAGER', 'EARLY', 'EARTH', 'EIGHT', 'ELITE', 'EMPTY', 'ENEMY', 'ENJOY', 'ENTER', 'ENTRY', 'EQUAL', 'ERROR', 'EVENT', 'EVERY', 'EXACT', 'EXIST', 'EXTRA', 'FAITH', 'FALSE', 'FAULT', 'FIBER', 'FIELD', 'FIFTH', 'FIFTY', 'FIGHT', 'FINAL', 'FIRST', 'FIXED', 'FLASH', 'FLEET', 'FLOOR', 'FLUID', 'FOCUS', 'FORCE', 'FORTH', 'FORTY', 'FORUM', 'FOUND', 'FRAME', 'FRANK', 'FRAUD', 'FRESH', 'FRONT', 'FRUIT', 'FULLY', 'FUNNY', 'GIANT', 'GIVEN', 'GLASS', 'GLOBE', 'GOING', 'GRACE', 'GRADE', 'GRAND', 'GRANT', 'GRASS', 'GREAT', 'GREEN', 'GROSS', 'GROUP', 'GROWN', 'GUARD', 'GUESS', 'GUEST', 'GUIDE', 'HAPPY', 'HARRY', 'HEART', 'HEAVY', 'HENCE', 'HENRY', 'HORSE', 'HOTEL', 'HOUSE', 'HUMAN', 'IDEAL', 'IMAGE', 'INDEX', 'INNER', 'INPUT', 'ISSUE', 'JAPAN', 'JIMMY', 'JOINT', 'JONES', 'JUDGE', 'KNOWN', 'LABEL', 'LARGE', 'LASER', 'LATER', 'LAUGH', 'LAYER', 'LEARN', 'LEASE', 'LEAST', 'LEAVE', 'LEGAL', 'LEVEL', 'LEWIS', 'LIGHT', 'LIMIT', 'LINKS', 'LIVES', 'LOCAL', 'LOGIC', 'LOOSE', 'LOWER', 'LUCKY', 'LUNCH', 'LYING', 'MAGIC', 'MAJOR', 'MAKER', 'MARCH', 'MARIA', 'MATCH', 'MAYBE', 'MAYOR', 'MEANT', 'MEDIA', 'METAL', 'MIGHT', 'MINOR', 'MINUS', 'MIXED', 'MODEL', 'MONEY', 'MONTH', 'MORAL', 'MOTOR', 'MOUNT', 'MOUSE', 'MOUTH', 'MOVIE', 'MUSIC', 'NEEDS', 'NEVER', 'NEWLY', 'NIGHT', 'NOISE', 'NORTH', 'NOTED', 'NOVEL', 'NURSE', 'OCCUR', 'OCEAN', 'OFFER', 'OFTEN', 'ORDER', 'OTHER', 'OUGHT', 'PAINT', 'PANEL', 'PAPER', 'PARTY', 'PEACE', 'PETER', 'PHASE', 'PHONE', 'PHOTO', 'PIECE', 'PILOT', 'PITCH', 'PLACE', 'PLAIN', 'PLANE', 'PLANT', 'PLATE', 'POINT', 'POUND', 'POWER', 'PRESS', 'PRICE', 'PRIDE', 'PRIME', 'PRINT', 'PRIOR', 'PRIZE', 'PROOF', 'PROUD', 'PROVE', 'QUEEN', 'QUICK', 'QUIET', 'QUITE', 'RADIO', 'RAISE', 'RANGE', 'RAPID', 'RATIO', 'REACH', 'READY', 'REFER', 'RIGHT', 'RIVAL', 'RIVER', 'ROBIN', 'ROGER', 'ROMAN', 'ROUGH', 'ROUND', 'ROUTE', 'ROYAL', 'RURAL', 'SCALE', 'SCENE', 'SCOPE', 'SCORE', 'SENSE', 'SERVE', 'SEVEN', 'SHALL', 'SHAPE', 'SHARE', 'SHARP', 'SHEET', 'SHELF', 'SHELL', 'SHIFT', 'SHIRT', 'SHOCK', 'SHOOT', 'SHORT', 'SHOWN', 'SIGHT', 'SINCE', 'SIXTH', 'SIXTY', 'SIZED', 'SKILL', 'SLEEP', 'SLIDE', 'SMALL', 'SMART', 'SMILE', 'SMITH', 'SMOKE', 'SOLID', 'SOLVE', 'SORRY', 'SOUND', 'SOUTH', 'SPACE', 'SPARE', 'SPEAK', 'SPEED', 'SPEND', 'SPENT', 'SPLIT', 'SPOKE', 'SPORT', 'STAFF', 'STAGE', 'STAKE', 'STAND', 'START', 'STATE', 'STEAM', 'STEEL', 'STICK', 'STILL', 'STOCK', 'STONE', 'STOOD', 'STORE', 'STORM', 'STORY', 'STRIP', 'STUCK', 'STUDY', 'STUFF', 'STYLE', 'SUGAR', 'SUITE', 'SUPER', 'SWEET', 'TABLE', 'TAKEN', 'TASTE', 'TAXES', 'TEACH', 'TEETH', 'TERRY', 'TEXAS', 'THANK', 'THEFT', 'THEIR', 'THEME', 'THERE', 'THESE', 'THICK', 'THING', 'THINK', 'THIRD', 'THOSE', 'THREE', 'THREW', 'THROW', 'TIGHT', 'TIMES', 'TIRED', 'TITLE', 'TODAY', 'TOPIC', 'TOTAL', 'TOUCH', 'TOUGH', 'TOWER', 'TRACK', 'TRADE', 'TRAIN', 'TREAT', 'TREND', 'TRIAL', 'TRIED', 'TRIES', 'TRUCK', 'TRULY', 'TRUST', 'TRUTH', 'TWICE', 'UNDER', 'UNDUE', 'UNION', 'UNITY', 'UNTIL', 'UPPER', 'UPSET', 'URBAN', 'USAGE', 'USUAL', 'VALID', 'VALUE', 'VIDEO', 'VIRUS', 'VISIT', 'VITAL', 'VOICE', 'WASTE', 'WATCH', 'WATER', 'WHEEL', 'WHERE', 'WHICH', 'WHILE', 'WHITE', 'WHOLE', 'WHOSE', 'WOMAN', 'WOMEN', 'WORLD', 'WORRY', 'WORSE', 'WORST', 'WORTH', 'WOULD', 'WOUND', 'WRITE', 'WRONG', 'WROTE', 'YIELD', 'YOUNG', 'YOUTH']
+            return jsonify(choice(word_list).lower())
+
+    # building RESTapi endpoint
+    api.add_resource(_Create, '/create')
+    api.add_resource(_Read, '/')
