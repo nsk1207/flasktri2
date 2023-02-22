bc1ddad36d65d666facfa05b5aef6b8436d8b602
disvs <sbsommers3@gmail.com>
2023-02-21 17:49:02 -0800

    modified:   api/app.py

--- api/app.py
+++ api/app.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 from flask_restful import Api, Resource # used for REST API building
 from datetime import datetime
 from random import choice
-from model.QATrivia import User
+
 
 user_api = Blueprint('user_api', __name__,
                    url_prefix='/api/users')
