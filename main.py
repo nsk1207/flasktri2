2796f38b6e5bf5df5a1ae7636fb57c769a50b040
disvs <sbsommers3@gmail.com>
2023-02-21 17:37:09 -0800

    modified:   main.py

--- main.py
+++ main.py
@@ -53,8 +53,7 @@ def activate_job():
 
 # this runs the application on the development server
 if __name__ == "__main__":
-    from flask_cors import CORS
-    cors = CORS(app)
+   
     # change name for testing
 
-    app.run(debug=True, host="0.0.0.0", port="4444")
+    app.run(debug=True, host="0.0.0.0", port="8086")
