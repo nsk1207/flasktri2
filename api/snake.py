28e048538afcb6dc6229ae265fd57fdd677ed984
disvs <sbsommers3@gmail.com>
2023-02-26 18:19:49 -0800

    modified:   api/snake.py

--- api/snake.py
+++ api/snake.py
@@ -54,7 +54,54 @@ class UserAPI:
             snakes = Snakes.query.all()    # read/extract all users from database
             json_ready = [snake.read() for snake in snakes]  # prepare output in json
             return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
+        
+    class _Update(Resource):
+        def put(self):
+            ''' Read data for json body '''
+            body = request.get_json()
+
+            ''' Avoid garbage in, error checking '''
+            # validate uid
+            uid = body.get('uid')
+            if uid is None or len(uid) < 2:
+                return {'message': f'User ID is missing, or is less than 2 characters'}, 210
+            # look for password and dob
+            new_snakescore = body.get('snakescore')
+            if new_snakescore is None:
+                return {'message': f'Snake score is missing'}, 210
+
+            ''' #1: Key code block, lookup USER OBJECT '''
+            snake = Snakes.query.filter_by(uid=uid).first()
+            if snake is None:
+                return {'message': f'User with User ID {uid} not found'}, 210
+
+            ''' Additional garbage error checking '''
+            # set password if provided
+
+            ''' #2: Key Code block to update user in database '''
+            # update user in database
+            snake.update(snakescore=new_snakescore)
+            # success returns json of updated user
+            return jsonify(snake.read())
+
+    class _Delete(Resource):
+        def delete(self):
+            ''' Read data for json body '''
+            body = request.get_json()
+
+            ''' Avoid garbage in, error checking '''
+            # validate uid
+            uid = body.get('uid')
+            if uid is None or len(uid) < 2:
+                return {'message': f'User ID is missing, or is less than 2 characters'}, 210
+
+            ''' #1: Key code block, lookup USER OBJECT '''
+            snake = Snakes.query.filter_by(uid=uid).first()
+            if snake is None:
+                return {'message': f'sorry! record doesnt exist'}
 
     # building RESTapi endpoint
     api.add_resource(_Create, '/create')
     api.add_resource(_Read, '/')
+    api.add_resource(_Update, '/update')
+    api.add_resource(_Delete, '/delete')
